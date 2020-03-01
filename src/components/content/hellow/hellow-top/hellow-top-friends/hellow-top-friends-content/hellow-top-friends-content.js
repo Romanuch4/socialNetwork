5e26bfbd46d90c7bc2cc938d71fc46518404a54d
Roman <pomankrentovsky@gmail.com>
2020-03-01 12:47:30 +0200

    Сделал функционал для блока "Дни рождения"

--- src/components/content/hellow/hellow-top/hellow-top-friends/hellow-top-friends-content/hellow-top-friends-content.js
+++ src/components/content/hellow/hellow-top/hellow-top-friends/hellow-top-friends-content/hellow-top-friends-content.js
@@ -5,23 +5,29 @@ import image2 from '../../../../../../images/Max.jpg';
 import image3 from '../../../../../../images/Karina.jpg';
 import imageDefault from './image-default/src-default.png';
 
-const HellowTopPFriendsContent = ({stateFriends}) => {
+const HellowTopPFriendsContent = ({ stateFriends }) => {
+  const date = new Date();
+  let birthday = `${date.getDate()}.${date.getMonth()}`;
   let src = '';
-  const elements = stateFriends.map( item => {
-    if(item.image === 'Roman.png') {
-      src = image1;
-    } else if(item.image === 'Max.jpg') {
-      src = image2;
-    } else if(item.image === 'Karina.jpg') {
-      src = image3;
+  const elements = stateFriends.map(item => {
+    if (item.birthday === birthday) {
+      if (item.image === 'Roman.png') {
+        src = image1;
+      } else if (item.image === 'Max.jpg') {
+        src = image2;
+      } else if (item.image === 'Karina.jpg') {
+        src = image3;
+      } else {
+        src = imageDefault;
+      }
+      return (
+        <div className="hellow-top-friends-content-item" key={item.id} data-online={item.online} data-birthday={item.birthday} title={item.name}>
+          <img className="hellow-top-friends-content-img" src={src} alt="friend" />
+        </div>
+      );
     } else {
-      src = imageDefault;
+      return undefined
     }
-    return (
-      <div className="hellow-top-friends-content-item" key={item.id} data-online={item.online} data-birthday={item.birthday} title={item.name}>
-        <img className="hellow-top-friends-content-img" src={src} alt="friend"/>
-      </div>
-    )
   });
 
   return (
--- src/state.js
+++ src/state.js
@@ -4,41 +4,60 @@ const Store = {
   _State: {
     friends: [
       {
-        id: 1, name: 'Roman', online: true, image: 'Roman.png', birthday: '7.1.2002'
+        id: 1, name: 'Roman', online: true, image: 'Roman.png', birthday: '1.2'
       },
       {
-        id: 2, name: 'Max', online: false, image: 'Max.jpg', birthday: '13.4.2000'
+        id: 2, name: 'Max', online: false, image: 'Max.jpg', birthday: '13.4'
       },
       {
-        id: 3, name: 'Karina', online: true, image: 'Karina.jpg', birthday: '14.5.2002'
+        id: 3, name: 'Karina', online: true, image: 'Karina.jpg', birthday: '1.2'
       },
       {
-        id: 4, name: 'Roman1', online: false, image: 'Roman.png', birthday: '8.1.1998'
+        id: 4, name: 'Roman1', online: false, image: 'Roman.png', birthday: '1.2'
       },
       {
-        id: 5, name: 'Max2', online: true, image: 'Max.jpg', birthday: '8.2.2002'
+        id: 5, name: 'Max2', online: true, image: 'Max.jpg', birthday: '8.2'
       },
       {
-        id: 6, name: 'Karina1', online: false, image: 'Karina.jpg', birthday: '7.1.2002'
+        id: 6, name: 'Karina1', online: false, image: 'Karina.jpg', birthday: '1.2'
       },
 
       {
-        id: 7, name: 'Roman2', online: true, image: 'Roman.png', birthday: '7.1.2002'
+        id: 7, name: 'Roman2', online: true, image: 'Roman.png', birthday: '7.1'
       },
       {
-        id: 8, name: 'Max2', online: false, image: 'Max.jpg', birthday: '13.4.2000'
+        id: 8, name: 'Max2', online: false, image: 'Max.jpg', birthday: '13.4'
       },
       {
-        id: 9, name: 'Karina2', online: true, image: 'Karina.jpg', birthday: '14.5.2002'
+        id: 9, name: 'Karina2', online: true, image: 'Karina.jpg', birthday: '14.5'
       },
       {
-        id: 10, name: 'Roman3', online: false, image: 'Roman.png', birthday: '8.1.1998'
+        id: 10, name: 'Roman3', online: false, image: 'Roman.png', birthday: '8.1'
       },
       {
-        id: 11, name: 'Max3', online: true, image: 'Max.jpg', birthday: '8.2.2002'
+        id: 11, name: 'Max3', online: true, image: 'Max.jpg', birthday: '8.2'
       },
       {
-        id: 12, name: 'Karina3', online: false, image: 'Karina.jpg', birthday: '7.1.2002'
+        id: 12, name: 'Karina3', online: false, image: 'Karina.jpg', birthday: '7.1'
+      },
+
+      {
+        id: 13, name: 'Roman2', online: true, image: 'Roman.png', birthday: '3.1'
+      },
+      {
+        id: 14, name: 'Max2', online: false, image: 'Max.jpg', birthday: '3.4'
+      },
+      {
+        id: 15, name: 'Karina2', online: true, image: 'Karina.jpg', birthday: '2.2'
+      },
+      {
+        id: 16, name: 'Roman3', online: false, image: 'Roman.png', birthday: '2.2'
+      },
+      {
+        id: 17, name: 'Max3', online: true, image: 'Max.jpg', birthday: '1.2'
+      },
+      {
+        id: 18, name: 'Karina3', online: false, image: 'Karina.jpg', birthday: '1.1'
       },
     ],
 
