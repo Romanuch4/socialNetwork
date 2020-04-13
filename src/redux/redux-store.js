e53ce13c79f01c6b0ae1efb53081d7057fe83973
Roman <pomankrentovsky@gmail.com>
2020-04-13 17:47:47 +0300

    Add google dev tools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import PostReducer from './reducers/post-reducer';
 import FriendsReducer from './reducers/friends-reducer';
 import deleteEventsReducer from './reducers/delete_events-reducer';
@@ -16,8 +17,13 @@ const reducers = combineReducers({
   auth: authReducer,
   app: appReducer,
   form: formReducer,
-})
+});
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+/* const store = createStore(reducers, applyMiddleware(thunkMiddleware)); */
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunkMiddleware),
+  // other store enhancers if any
+));
 
 export default store;
