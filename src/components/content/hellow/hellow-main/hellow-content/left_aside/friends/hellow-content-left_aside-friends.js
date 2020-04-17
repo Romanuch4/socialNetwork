4a43bdb510e8e78e16947350490ff5d23448bd36
Roman <pomankrentovsky@gmail.com>
2020-04-17 19:13:25 +0300

    filename is too long

--- src/components/content/hellow/hellow-main/hellow-content/left_aside/friends/hellow-content-left_aside-friends.js
+++ src/components/content/hellow/hellow-main/hellow-content/left_aside/friends/hellow-content-left_aside-friends.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './hellow-content-left_aside-friends.css';
-import LeftAsideFriendsItemContainer from './hellow-content-left_aside-friends-item';
-import LeftAsideFriendsSearchContainer from './hellow-content-left_aside-friends-search';
+import LeftAsideFriendsItemContainer from './friends-item';
+import LeftAsideFriendsSearchContainer from './friends-search';
 
 const LeftAsideFriends = ({dispatch}) => {
   return (
